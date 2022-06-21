921f408fbc3fc717317b1c27699c39f48f8a3bd4
Kato <45391880+kat0h@users.noreply.github.com>
2022-06-21 23:18:31 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { readAll } from "https://deno.land/std@0.144.0/streams/mod.ts";
 import * as _ from "./wasm_exec.js"
 
 let go = new window.Go()
-let inst = await WebAssembly.instantiate(Deno.readFileSync("./y2j.wasm"), go.importObject)
+let inst = await WebAssembly.instantiate(Deno.readFileSync(new URL("./y2j.wasm", import.meta.url)), go.importObject)
 go.run(inst.instance)
 
 
